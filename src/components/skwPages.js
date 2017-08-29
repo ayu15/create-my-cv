1284ca8528e99f16768f2e23d4d909a30d999cd2
Ayush <ayush15may@gmail.com>
2017-08-29 23:48:16 +0100

    performance hacks3

--- src/components/skwPages.js
+++ src/components/skwPages.js
@@ -21,47 +21,47 @@ export default class SkwPages extends React.Component {
 		this.pagination = this.pagination.bind(this);
 		this.navigateUp = this.navigateUp.bind(this);
 		this.navigateDown = this.navigateDown.bind(this);
+	}
 
-		window.onload = function () {
-			(function (d) {
-				var
-					ce = function (e, n) {
-						var a = document.createEvent("CustomEvent");
-						a.initCustomEvent(n, true, true, e.target);
-						e.target.dispatchEvent(a);
-						a = null;
-						return false
+	customSwipeEvent() {
+		(function (d) {
+			var
+				ce = function (e, n) {
+					var a = document.createEvent("CustomEvent");
+					a.initCustomEvent(n, true, true, e.target);
+					e.target.dispatchEvent(a);
+					a = null;
+					return false
+				},
+				nm = true, sp = {x: 0, y: 0}, ep = {x: 0, y: 0},
+				touch = {
+					touchstart: function (e) {
+						sp = {x: e.touches[0].pageX, y: e.touches[0].pageY}
 					},
-					nm = true, sp = {x: 0, y: 0}, ep = {x: 0, y: 0},
-					touch = {
-						touchstart: function (e) {
-							sp = {x: e.touches[0].pageX, y: e.touches[0].pageY}
-						},
-						touchmove: function (e) {
-							nm = false;
-							ep = {x: e.touches[0].pageX, y: e.touches[0].pageY}
-						},
-						touchend: function (e) {
-							if (nm) {
-								ce(e, 'fc')
-							} else {
-								var x = ep.x - sp.x, xr = Math.abs(x), y = ep.y - sp.y, yr = Math.abs(y);
-								if (Math.max(xr, yr) > 20) {
-									ce(e, (xr > yr ? (x < 0 ? 'swl' : 'swr') : (y < 0 ? 'swu' : 'swd')))
-								}
+					touchmove: function (e) {
+						nm = false;
+						ep = {x: e.touches[0].pageX, y: e.touches[0].pageY}
+					},
+					touchend: function (e) {
+						if (nm) {
+							ce(e, 'fc')
+						} else {
+							var x = ep.x - sp.x, xr = Math.abs(x), y = ep.y - sp.y, yr = Math.abs(y);
+							if (Math.max(xr, yr) > 20) {
+								ce(e, (xr > yr ? (x < 0 ? 'swl' : 'swr') : (y < 0 ? 'swu' : 'swd')))
 							}
-							;
-							nm = true
-						},
-						touchcancel: function (e) {
-							nm = false
 						}
-					};
-				for (var a in touch) {
-					d.addEventListener(a, touch[a], false);
-				}
-			})(document);
-		}
+						;
+						nm = true
+					},
+					touchcancel: function (e) {
+						nm = false
+					}
+				};
+			for (var a in touch) {
+				d.addEventListener(a, touch[a], false);
+			}
+		})(document);
 	}
 
 	pagination() {
@@ -86,12 +86,13 @@ export default class SkwPages extends React.Component {
 	componentDidMount() {
 		let screenWidth = window.matchMedia("(min-width: 600px)");
 		if (screenWidth.matches) {
+			this.customSwipeEvent();
+			this.skwPages.addEventListener('wheel', this.handleScroll, {passive: true});
+			document.body.addEventListener('keydown', this.handleArrowScroll, {passive: true});
 			document.body.addEventListener('swl', this.navigateUp, {passive: true});
 			document.body.addEventListener('swr', this.navigateDown, {passive: true});
 			document.body.addEventListener('swu', this.navigateDown, {passive: true});
 			document.body.addEventListener('swd', this.navigateUp, {passive: true});
-			document.body.addEventListener('keydown', this.handleArrowScroll, {passive: true});
-			this.skwPages.addEventListener('wheel', this.handleScroll, {passive: true});
 		}
 	}
 
